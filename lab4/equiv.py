6a52cac233e0ce80c22e1edeb5d3ae7fd9e929ef
Aurutus <emslhy@hotmail.com>
2023-12-21 13:55:53 +0800

    lab: lab4 xrcs 2

--- lab4/equiv.py
+++ lab4/equiv.py
@@ -26,10 +26,19 @@ int power3_new(int in){
 # that is:
 # P1/\P2 -> out_a==out_b
 
-# Exercise 2: try to prove the algorithm 'power3' and 'power3_new' 
+# Exercise 2: try to prove the algorithm 'power3' and 'power3_new'
 # are equivalent by using EUF theory
 # Please construct, manually, the propositions P1 and P2, and let Z3
 # prove the above implication. (Note that you don't need to generate
 # P1 or P2 automatically, just write down them manually.)
-raise NotImplementedError('TODO: Your code here!') 
 
+ist = IntSort()
+multi = Function("multi", ist, ist, ist)
+_in, _out_a_1, _out_a_2, _out_b = Consts(
+    "_in, _out_a_1, _out_a_2, _out_b", ist)
+
+power3 = And(_out_a_1 == multi(_in, _in), And(
+    _out_a_2 == multi(_out_a_1, _in)))
+power3_new = _out_b == multi(_in, multi(_in, _in))
+
+solve(power3, power3_new, _out_a_2 == _out_b)
