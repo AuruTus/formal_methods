a4c477cf593ab53d54b3dade5aba5d3e305d535b
Aurutus <emslhy@hotmail.com>
2023-12-31 20:39:14 +0800

    refactor: simplify queen code

--- lab3/n-queen-go/main.go
+++ lab3/n-queen-go/main.go
@@ -9,14 +9,9 @@ const N = 32
 
 func n_queen_partial(x, y int) int {
 	cnt := 0
-	board := [N]int{}
 	col_m := [N]int8{}
 	diag_m := [2*N - 1]int8{}
 	anti_diag_m := [2*N - 1]int8{}
-
-	for i := range board {
-		board[i] = -1
-	}
 
 	var dfs func(x, y int)
 	dfs = func(x, y int) {
@@ -26,16 +21,9 @@ func n_queen_partial(x, y int) int {
 			}
 			return
 		}
-		if col_m[y] != 0 {
+		if col_m[y] != 0 || diag_m[x-y+N-1] != 0 || anti_diag_m[x+y] != 0 {
 			return
 		}
-		if diag_m[x-y+N-1] != 0 {
-			return
-		}
-		if anti_diag_m[x+y] != 0 {
-			return
-		}
-		board[x] = y
 		col_m[y] = 1
 		diag_m[x-y+N-1] = 1
 		anti_diag_m[x+y] = 1
@@ -45,7 +33,6 @@ func n_queen_partial(x, y int) int {
 		anti_diag_m[x+y] = 0
 		diag_m[x-y+N-1] = 0
 		col_m[y] = 0
-		board[x] = -1
 	}
 
 	dfs(x, y)
--- lab3/n-queen/src/main.rs
+++ lab3/n-queen/src/main.rs
@@ -1,23 +1,27 @@
 #![feature(generic_const_exprs)]
 
+#[derive(PartialEq, Clone, Copy)]
+enum Existence {
+    Exist,
+    None,
+}
+
 fn n_queen<const N: usize>()
 where
     [(); 2 * N - 1]:,
 {
     let mut cnt: i128 = 0;
-    let board = [-1; N];
-    let col_m = [0 as i8; N];
-    let diag_m = [0 as i8; 2 * N - 1];
-    let anti_diag_m = [0 as i8; 2 * N - 1];
+    let col_m = [Existence::None; N];
+    let diag_m = [Existence::None; 2 * N - 1];
+    let anti_diag_m = [Existence::None; 2 * N - 1];
 
     fn _dfs<const N: usize>(
         x: i32,
         y: i32,
         cnt: &mut i128,
-        mut board: [i32; N],
-        mut col_m: [i8; N],
-        mut diag_m: [i8; 2 * N - 1],
-        mut anti_diag_m: [i8; 2 * N - 1],
+        mut col_m: [Existence; N],
+        mut diag_m: [Existence; 2 * N - 1],
+        mut anti_diag_m: [Existence; 2 * N - 1],
     ) {
         if x >= N as i32 {
             if y == 0 {
@@ -25,35 +29,38 @@ where
             };
             return;
         }
-        if col_m[y as usize] != 0 {
+
+        let _y = y as usize;
+        let _diag = (x - y + N as i32 - 1) as usize;
+        let _anti_diag = (x + y) as usize;
+
+        if col_m[_y] != Existence::None
+            || diag_m[_diag] != Existence::None
+            || anti_diag_m[_anti_diag] != Existence::None
+        {
             return;
         }
-        if diag_m[(x - y + N as i32 - 1) as usize] != 0 {
-            return;
-        }
-        if anti_diag_m[(x + y) as usize] != 0 {
-            return;
-        }
-        board[x as usize] = y;
-        col_m[y as usize] = 1;
-        diag_m[(x - y + N as i32 - 1) as usize] = 1;
-        anti_diag_m[(x + y) as usize] = 1;
+
+        col_m[_y] = Existence::Exist;
+        diag_m[_diag] = Existence::Exist;
+        anti_diag_m[_anti_diag] = Existence::Exist;
         for _y in 0..N as i32 {
-            _dfs(x + 1, _y, cnt, board, col_m, diag_m, anti_diag_m);
+            _dfs(x + 1, _y, cnt, col_m, diag_m, anti_diag_m);
         }
-        anti_diag_m[(x + y) as usize] = 0;
-        diag_m[(x - y + N as i32 - 1) as usize] = 0;
-        col_m[y as usize] = 0;
-        board[x as usize] = -1;
+        anti_diag_m[_anti_diag] = Existence::None;
+        diag_m[_diag] = Existence::None;
+        col_m[_y] = Existence::None;
     }
 
     for y in 0..N as i32 {
-        _dfs(0, y, &mut cnt, board, col_m, diag_m, anti_diag_m);
+        _dfs(0, y, &mut cnt, col_m, diag_m, anti_diag_m);
     }
 
     println!("{cnt}");
 }
 
 fn main() {
+    // n_queen::<10>();
     n_queen::<16>();
+    // n_queen::<32>();
 }
