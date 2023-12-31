a27385bc4e841b42af5c0e27cfab6b5b882d0757
Aurutus <emslhy@hotmail.com>
2023-12-31 12:56:06 +0800

    lab: lab5 partB SSP

--- lab5/subset_sum.py
+++ lab5/subset_sum.py
@@ -37,7 +37,7 @@ def subset_sum_la(target_set: list):
     # @Exercise 9: please fill in the missing src to add
     # the following constraint into the solver.
     #       \sum_i flags[i]*target_set[i] = 0
-    raise NotImplementedError('TODO: Your code here!') 
+    solver.add(sum([f*r for (f, r) in zip(flags, target_set)]) == 0)
 
     start = time.time()
     result = solver.check()
@@ -64,7 +64,8 @@ def subset_sum_la_opt(target_set: list):
     solver.add(PbGe([(flags[i], 1) for i in range(len(target_set))], 1))
 
     # selected set must sum to zero
-    solver.add(PbEq([(flags[i], target_set[i]) for i in range(len(target_set))], 0))
+    solver.add(PbEq([(flags[i], target_set[i])
+               for i in range(len(target_set))], 0))
 
     start = time.time()
     result = solver.check()
@@ -76,19 +77,47 @@ def subset_sum_la_opt(target_set: list):
 
 
 # dynamic programming-based (DP) solution (don't confuse DP with LP):
-def subset_sum_dp(target_set):
-    def subset_sum_dp_do(the_set, target, index):
+def subset_sum_dp(target_set) -> bool:
+    from functools import cache
+
+    @cache
+    def subset_sum_dp_do(target, index) -> bool:
         if index == 0:
             return False
-        if target == the_set[index - 1]:
+        if target == target_set[index - 1]:
             return True
-        if subset_sum_dp_do(the_set, target, index - 1):
+        if subset_sum_dp_do(target, index - 1):
             return True
-        return subset_sum_dp_do(the_set, target - the_set[index - 1], index - 1)
+        return subset_sum_dp_do(target - target_set[index - 1], index - 1)
 
     start = time.time()
-    result = subset_sum_dp_do(target_set, 0, len(target_set))
+    result = subset_sum_dp_do(0, len(target_set))
     print(f"time used in DP: {(time.time() - start):.6f}s")
+    return result
+
+
+def subset_sum_dp_opt(tartget_set) -> bool:
+    def _subset_sum_dp_opt() -> bool:
+        dp = {}
+        queue = [(-1, 0)]
+        l = len(tartget_set)
+        while len(queue) > 0:
+            curr = queue.pop(0)
+            for i in range(curr[0]+1, l):
+                queue.append((i, curr[1]))
+                if not dp.get(curr[1] + tartget_set[i]):
+                    if curr[1] + tartget_set[i] == 0:
+                        return True
+                    queue.append((i, curr[1] + tartget_set[i]))
+                    dp[curr[1] + tartget_set[i]] = True
+
+        return False
+
+    start = time.time()
+    result = _subset_sum_dp_opt()
+    end = time.time()
+    print(f"time used in non-recursive DP: {end - start:.6f}s")
+
     return result
 
 
@@ -102,17 +131,98 @@ def gen_large_test(n):
 
 if __name__ == '__main__':
     # a small test case
+    print("---------------small true case---------------")
     small_set = [-7, -3, -2, 9000, 5, 8]
-    subset_sum_dp(small_set)
+    print(subset_sum_dp(small_set))
+    print(subset_sum_dp_opt(small_set))
     print(subset_sum_la(small_set))
     print(subset_sum_la_opt(small_set))
 
-    # a large test case
-    max_nums = 20
-    large_set = gen_large_test(max_nums)
+    '''
+    time used in DP: 0.000032s
+    True
+    time used in non-recursive DP: 0.000051s
+    True
+    time used in LA: 0.021428s
+    (True, [-3, -2, 5])
+    time used in LA optimized: 0.010312s
+    (True, [-3, -2, 5])
+    '''
+
+    print("---------------small false case---------------")
+    small_set = [-7, -3, -2, 9000, 8]
+    print(subset_sum_dp(small_set))
+    print(subset_sum_dp_opt(small_set))
+    print(subset_sum_la(small_set))
+    print(subset_sum_la_opt(small_set))
+
+    '''
+    time used in DP: 0.000392s
+    False
+    time used in non-recursive DP: 0.000205s
+    False
+    time used in LA: 0.015244s
+    (False, unsat)
+    time used in LA optimized: 0.011689s
+    (False, unsat)
+    '''
 
     # @Exercise 10: compare the efficiency of the DP and the
     # LP algorithm, by changing the value of "max_nums" to other
     # values, say, 200, 2000, 20000, 200000, ...
     # what's your observation? What conclusion you can draw from these data?
-    raise NotImplementedError('TODO: Your code here!') 
+
+    print("---------------max_nums = 20---------------")
+    max_nums = 20
+    large_set = gen_large_test(max_nums)
+    print(subset_sum_dp(large_set))
+    print(subset_sum_dp_opt(small_set))
+    print(subset_sum_la(large_set))
+    print(subset_sum_la_opt(large_set))
+
+    '''
+    time used in DP: 0.001588s
+    True
+    time used in non-recursive DP: 0.000133s
+    False
+    time used in LA: 0.071900s
+    (True, [1, -1])
+    time used in LA optimized: 0.025063s
+    (True, [1, -1])
+    '''
+
+    print("---------------max_nums = 200--------------")
+    max_nums = 200
+    large_set = gen_large_test(max_nums)
+    print(subset_sum_dp(large_set))
+    print(subset_sum_dp_opt(small_set))
+    print(subset_sum_la(large_set))
+    print(subset_sum_la_opt(large_set))
+
+    '''
+    time used in DP: 0.027842s
+    True
+    time used in non-recursive DP: 0.000149s
+    False
+    time used in LA: 0.187291s
+    (True, [1, -1])
+    time used in LA optimized: 0.019695s
+    (True, [1, -1])
+    '''
+
+    print("---------------max_nums = 2000-------------")
+    max_nums = 2000
+    large_set = gen_large_test(max_nums)
+    # print(subset_sum_dp(large_set)) # RecursionError: maximum recursion depth exceeded in comparison
+    print(subset_sum_dp_opt(small_set))
+    print(subset_sum_la(large_set))
+    print(subset_sum_la_opt(large_set))
+
+    '''
+    time used in non-recursive DP: 0.000087s
+    False
+    time used in LA: 45.825885s
+    (True, [1, -1])
+    time used in LA optimized: 0.763517s
+    (True, [1, -1])
+    '''
